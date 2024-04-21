b751784e7c52cbc6390b9064eb0715145662f07f
Samira Silva <samira.e.b.silva@gmail.com>
2024-04-21 17:18:42 -0400

    edit

---
+++ Javascript/8kyu/easy_logs.js
@@ -0,0 +1,8 @@
+// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+
+// log x A+ log X B
+
+function logs(x , a, b){
+    //your code here
+      return Math.log(a * b) / Math.log(x);
+    }
