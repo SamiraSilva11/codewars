36fe013df46290115c2f37c854a9e5512c2c1e93
Samira Silva <samira.e.b.silva@gmail.com>
2024-04-25 19:19:55 -0400

    added kata 2024-04-25

---
+++ Javascript/8kyu/are_there_any_arrows_left?.js
@@ -0,0 +1,20 @@
+// Check your arrows
+
+// You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+// You need to verify that you have some good ones left, in order to prepare for battle:
+
+// anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+// If an arrow in the quiver does not have a damaged status, it means it's new.
+
+// The expected result is a boolean, indicating whether you have any good arrows left.
+
+function anyArrows(arrows) {
+    // Iterate through each arrow object
+    for (const arrow of arrows) {
+      // Check if the arrow is not damaged
+      if (!arrow.damaged) {
+        return true; // Return true if an undamaged arrow is found
+      }
+    }
+    return false; // Return false if no undamaged arrow is found
+  }
