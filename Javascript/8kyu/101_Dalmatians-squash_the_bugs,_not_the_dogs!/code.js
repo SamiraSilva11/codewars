92699be7c61de1519f5a4389e79c2399701ceef6
Samira Silva <samira.e.b.silva@gmail.com>
2024-05-03 23:10:49 -0400

    edit

--- Javascript/8kyu/101_Dalmatians-squash_the_bugs,_not_the_dogs!/code.js
+++ Javascript/8kyu/101_Dalmatians-squash_the_bugs,_not_the_dogs!/code.js
@@ -1,14 +1,3 @@
-// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, 
-// and you simply do not know how to respond!
-
-// By repairing the function provided, you will find out exactly how you should respond, depending on the 
-// number of dogs he has.
-
-// The number of dogs will always be a number and there will always be at least 1 dog.
-
-// Good luck!
-
-
 function howManyDalmatians(number){
   
     var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
