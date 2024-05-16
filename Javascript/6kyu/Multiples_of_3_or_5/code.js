b5c2f5f7fe7e5128cbfa5a71b29c5fa23f280329
Samira Silva <samira.e.b.silva@gmail.com>
2024-05-16 00:35:08 -0400

    added kata 2024-05-16

---
+++ Javascript/6kyu/Multiples_of_3_or_5/code.js
@@ -0,0 +1,13 @@
+function solution(number){
+    let sum = 0;
+    if(number <= 0){
+      return 0
+    }else {
+      for (let i=0; i<number; i++) {
+        if(i % 3 === 0 || i% 5 ===0){
+          sum+=i;
+        }
+      }
+      return sum;
+    }
+  }
