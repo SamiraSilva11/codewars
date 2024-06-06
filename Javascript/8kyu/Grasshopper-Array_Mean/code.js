3b9cfbf4da1080f5ab96024ace4708744d272c1c
Samira Silva <samira.e.b.silva@gmail.com>
2024-06-06 17:33:40 -0400

    added kata 2024-06-06

---
+++ Javascript/8kyu/Grasshopper-Array_Mean/code.js
@@ -0,0 +1,13 @@
+var findAverage = function (nums) {
+    if (nums.length === 0) {
+      return 0; // Return 0 for an empty array
+    }
+    
+    // Calculate the sum of all numbers in the array
+    const sum = nums.reduce((acc, curr) => acc + curr, 0);
+    
+    // Calculate the mean by dividing the sum by the number of elements in the array
+    const mean = sum / nums.length;
+    
+    return mean;
+  }
