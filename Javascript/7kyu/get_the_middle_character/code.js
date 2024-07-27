daef8b9e5750fa2a66399c99e71f0e390d7bc23d
Samira Silva <samira.e.b.silva@gmail.com>
2024-07-27 00:22:34 -0400

    edit

--- Javascript/7kyu/get_the_middle_character/code.js
+++ Javascript/7kyu/get_the_middle_character/code.js
@@ -11,3 +11,11 @@ function getMiddle(s)
         return s.charAt(mid);
     }
 }
+
+
+// best practice
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
