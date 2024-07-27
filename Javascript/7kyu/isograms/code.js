961c9c1a0c275bb369e3d021320428fd7e8c9dbc
Samira Silva <samira.e.b.silva@gmail.com>
2024-07-27 00:41:46 -0400

    edit

--- Javascript/7kyu/isograms/code.js
+++ Javascript/7kyu/isograms/code.js
@@ -2,3 +2,8 @@ const isIsogram = str => {
   str = str.toLowerCase().replace(/\s+/g, ''); // Convert to lowercase and remove spaces
   return new Set(str).size === str.length; // Compare the size of the Set with the string length
 };
+
+//best practice
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
